11463044bf55f33b25eb696ee16bcb0692740f6a
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2024-02-26 19:42:14 -0500

    including a map with a leafleat library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
   const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      Map
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.mapContainer}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
